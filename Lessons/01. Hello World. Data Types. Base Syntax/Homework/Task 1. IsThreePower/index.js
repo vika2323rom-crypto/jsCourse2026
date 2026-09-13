50be7fa78bf7bbcb12ae3a979e806009af461ae7
victoriaromanova <vika2323rom@gmail.com>
2026-09-13 11:15:28 +0300

    Добавила функцию из дз isThreePower и класссную работу Hello World!

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
@@ -1,7 +1,16 @@
 const isThreePower = (num) => {
-    // Начало
+    if (num <= 0) {
+        return false;
+    }
+    while(num % 3 === 0) {
+        num = num / 3;
+    }
+    return num === 1;
 
-    // Конец
 };
-
+console.log(isThreePower(1)); // true
+console.log(isThreePower(2)); // false
+console.log(isThreePower(27)); // true
+console.log(isThreePower(0)); // false
+console.log(isThreePower(-3)); // false
 export default isThreePower;
--- Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 0. HelloWorld/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Tasks/Task 0. HelloWorld/index.js
@@ -1,5 +1,5 @@
 const helloWorld = () => {
-
+    return 'Hello World!';
 };
 
 export default helloWorld;
